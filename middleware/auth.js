ca3828c67b2cbb69ee939f696709d8974bd2c348
Piyush Yadav <84402719+piyushyadav0191@users.noreply.github.com>
2023-05-05 15:52:26 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,9 +5,6 @@ export const isAuthenticated = async (req, res, next) => {
   try {
     const { token } = req.cookies;
 
-    if (!token) {
-      return res.status(401).json({ success: false, message: "Login First" });
-    }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
